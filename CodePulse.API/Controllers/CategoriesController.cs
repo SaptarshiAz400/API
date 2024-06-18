12e73c98f2db31c6257e7d58d3bb83ea95f6bdc1
saptarshi <saptarshiAz400@outlook.com>
2024-06-18 06:58:58 +0000

    remove commnts

--- CodePulse.API/Controllers/CategoriesController.cs
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CodePulse.API.Controllers
 {
-    // https://localhost:xxxx/api/categories
+    
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
